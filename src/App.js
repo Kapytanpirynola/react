30587a0268e78abb9f7703729996223c003b3598
Adrian Mata <cnevarez751@gmail.com>
2024-02-26 19:46:41 -0600

    nuevo diseño

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
-import React, { useState } from 'react';
-import MovieList from './MovieList';
-import SearchBar from './SearchBar';
-import GenreFilter from './GenreFilter';
-import Footer from './footer'
-import Navbar from './navbar';
-import './navbar.css';
-import './footer.css';
-import './App.css'; 
-import './GenreFilter.css'; 
-import './SearchBar.css';
-
-
-// Define moviesData
-const moviesData = [
-  { id: 1, title: 'Scarface', description: 'A gripping tale of power, greed, and betrayal in the world of organized crime.', year: 1983, genre: 'Crime/Drama' },
-  { id: 2, title: 'Goodfellas', description: 'Follows the rise and fall of a young man\'s journey into the mafia underworld.', year: 1990, genre: 'Crime/Drama' },
-  { id: 3, title: 'The Departed', description: 'A tense thriller about undercover agents and double-crosses within the Boston criminal underworld.', year: 2006, genre: 'Crime/Drama' },
-  { id: 4, title: 'Pulp Fiction', description: 'A nonlinear narrative intertwining the lives of various characters in the criminal underworld.', year: 1994, genre: 'Crime/Drama' },
-  { id: 5, title: 'The Godfather: Part II', description: 'Explores the complexities of power and family within the Italian-American mafia.', year: 1974, genre: 'Crime/Drama' },
-  { id: 6, title: 'Heat', description: 'A cat-and-mouse game between a seasoned detective and a skilled thief planning the ultimate heist.', year: 1995, genre: 'Crime/Drama' },
-  { id: 7, title: 'Casino', description: 'Chronicles the rise and fall of a Las Vegas casino boss and his tumultuous relationships.', year: 1995, genre: 'Crime/Drama' },
-  { id: 8, title: 'The Untouchables', description: 'Eliot Ness and his team\'s pursuit of Al Capone during the Prohibition era.', year: 1987, genre: 'Crime/Drama' },
-  { id: 9, title: 'Reservoir Dogs', description: 'A heist gone wrong leads to suspicion and betrayal among a group of criminals.', year: 1992, genre: 'Crime/Drama' },
-  { id: 10, title: 'American Gangster', description: 'Based on the true story of a Harlem drug lord and the detective determined to bring him down.', year: 2007, genre: 'Crime/Drama' }
-];
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import YouTube from 'react-youtube';
 
 function App() {
-  const [movies] = useState(moviesData);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedGenre, setSelectedGenre] = useState('');
+  const API_URL = 'https://api.themoviedb.org/3'
+  const API_KEY = 'b586aeeb6751dae9b4be864b17537cc0'
+  const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+  const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
 
-  const filteredMovies = movies.filter(movie => 
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
-    (selectedGenre === '' || movie.genre === selectedGenre)
-  );
+
+  // variables de estado 
+  const[movies, setMovies] = useState([])
+  const [searchKey, setSearchKey] = useState("")
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movie"});
+  const [playing, setPlaying] = useState(false);
+
+  // funcion para realizar la peticion por get a la api
+  const fetchMovies = async(searchKey) => {
+    const type =  searchKey ? "search" : "discover"
+    const {data : { result },
+  } = await axios.get(`${API_URL}/${type}/movie`, {
+    params: {
+      api_key: API_KEY,
+      query : searchKey,
+    },
+  });
+
+  setMovies(result)
+  setMovie(result[0])
+
+  }
+
+  useEffect(()=>{
+    fetchMovies();
+  },[])
 
   return (
     <div>
-      <SearchBar/>
-      <div>
-      <Navbar/>
+      {/*contenedor de los posters de las pelicuals*/}
+      <div className='container mt-3'>
+        <div className='row'>
+          {movies.map((movie) =>(
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img src={`${URL_IMAGE + movie.poster_path}`}  alt="" height={600} width="100%"/>
+              <h4 className='text_center'>{movie.title}</h4>
+            </div>
+          ))}
+        </div>
       </div>
-      <GenreFilter/>
-      <div>
-      </div>
-      <h1 className='title'>Movie List</h1>
-      <div>
-      <MovieList movies={filteredMovies} />
-
-      </div>
-      <Footer/>
     </div>
   );
 }
